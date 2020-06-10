75f726d3ffd12c1fe677acfb6684ebe380453a9e
rajeshkannan <rajeshkannanrajagopal94@gmail.com>
2020-06-10 19:30:07 +0530

    Inital testing

---
+++ precache-manifest.688b6e986bb9779825e329fbaa342c67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf73ec4a8dc2cdf168190adc5c5e9a3",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "e9622f50a934e23d12a5",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "2fbd70a7e2f37c52cf12",
+    "url": "/material-dashboard-react/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "e9622f50a934e23d12a5",
+    "url": "/material-dashboard-react/static/js/2.7111f515.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/material-dashboard-react/static/js/2.7111f515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbd70a7e2f37c52cf12",
+    "url": "/material-dashboard-react/static/js/main.619d8cd5.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/material-dashboard-react/static/js/main.619d8cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe816d7b3554542361f",
+    "url": "/material-dashboard-react/static/js/runtime-main.2c4f3a29.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "09425d3249e4b2c8db93b60f6a7fa983",
+    "url": "/material-dashboard-react/static/media/sidebar-2.09425d32.jpg"
+  }
+]);
